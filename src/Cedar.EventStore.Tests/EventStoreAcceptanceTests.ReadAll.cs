ddeb8c454e3a1118fd7549d99b268112b2c87515
Damian Hickey <dhickey@gmail.com>
2015-05-18 18:37:41 +0200

    Read all backwards failing test.

--- src/Cedar.EventStore.Tests/EventStoreAcceptanceTests.ReadAll.cs
+++ src/Cedar.EventStore.Tests/EventStoreAcceptanceTests.ReadAll.cs
@@ -8,7 +8,7 @@
     public abstract partial class EventStoreAcceptanceTests
     {
         [Fact]
-        public async Task Can_read_all()
+        public async Task Can_read_all_forwards()
         {
             using (var fixture = GetFixture())
             {
@@ -38,6 +38,51 @@
 
                     count.Should().BeLessThan(20);
                     allEventsPage.Direction.Should().Be(ReadDirection.Forward);
+                    allEventsPage.IsEnd.Should().BeTrue();
+
+                    streamEvents.ShouldAllBeEquivalentTo(
+                        expectedEvents,
+                         options =>
+                         {
+                             options.Excluding(streamEvent => streamEvent.Checkpoint);
+                             options.Excluding(streamEvent => streamEvent.Created);
+                             return options;
+                         });
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Can_read_all_backward()
+        {
+            using (var fixture = GetFixture())
+            {
+                using (var eventStore = await fixture.GetEventStore())
+                {
+                    await eventStore.AppendToStream("stream-1", ExpectedVersion.NoStream, CreateNewStreamEvents(1, 2, 3));
+                    await eventStore.AppendToStream("stream-2", ExpectedVersion.NoStream, CreateNewStreamEvents(4, 5, 6));
+                    var expectedEvents = new[]
+                    {
+                        ExpectedStreamEvent("stream-1", 1, 0, fixture.GetUtcNow().UtcDateTime),
+                        ExpectedStreamEvent("stream-1", 2, 1, fixture.GetUtcNow().UtcDateTime),
+                        ExpectedStreamEvent("stream-1", 3, 2, fixture.GetUtcNow().UtcDateTime),
+                        ExpectedStreamEvent("stream-2", 4, 0, fixture.GetUtcNow().UtcDateTime),
+                        ExpectedStreamEvent("stream-2", 5, 1, fixture.GetUtcNow().UtcDateTime),
+                        ExpectedStreamEvent("stream-2", 6, 2, fixture.GetUtcNow().UtcDateTime)
+                    };
+
+                    var allEventsPage = await eventStore.ReadAll(null, 4);
+                    List<StreamEvent> streamEvents = new List<StreamEvent>(allEventsPage.StreamEvents);
+                    int count = 0;
+                    while (!allEventsPage.IsEnd && count < 20) //should not take more than 20 iterations.
+                    {
+                        allEventsPage = await eventStore.ReadAll(allEventsPage.NextCheckpoint, 10, ReadDirection.Backward);
+                        streamEvents.AddRange(allEventsPage.StreamEvents);
+                        count++;
+                    }
+
+                    count.Should().BeLessThan(20);
+                    allEventsPage.Direction.Should().Be(ReadDirection.Backward);
                     allEventsPage.IsEnd.Should().BeTrue();
 
                     streamEvents.ShouldAllBeEquivalentTo(

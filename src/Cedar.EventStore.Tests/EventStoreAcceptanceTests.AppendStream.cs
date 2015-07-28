2b1bfd474935418c38e8741f16281fcb71e0ba77
Damian Hickey <dhickey@gmail.com>
2015-07-28 12:52:36 +0200

    Add an idempotency scenario.

--- src/Cedar.EventStore.Tests/EventStoreAcceptanceTests.AppendStream.cs
+++ src/Cedar.EventStore.Tests/EventStoreAcceptanceTests.AppendStream.cs
@@ -45,6 +45,25 @@
         }
 
         [Fact]
+        public async Task When_append_stream_second_time_with_no_stream_expected_and_same_inital_event_then_should_not_throw()
+        {
+            // Idempotency
+            using (var fixture = GetFixture())
+            {
+                using (var eventStore = await fixture.GetEventStore())
+                {
+                    const string streamId = "stream-1";
+                    await eventStore
+                        .AppendToStream(streamId, ExpectedVersion.NoStream, CreateNewStreamEvents(1, 2, 3));
+
+                    await eventStore
+                        .AppendToStream(streamId, ExpectedVersion.NoStream, CreateNewStreamEvents(1, 2))
+                        .ShouldNotThrow();
+                }
+            }
+        }
+
+        [Fact]
         public async Task When_append_with_wrong_expected_version_then_should_throw()
         {
             using (var fixture = GetFixture())

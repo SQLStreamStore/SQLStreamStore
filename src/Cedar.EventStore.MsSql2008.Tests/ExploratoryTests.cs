71649e40cb91e5e30f80679d2d1863eac2380ff9
Damian Hickey <dhickey@gmail.com>
2016-01-04 00:23:55 +0100

    I *think* I have a nice thread safe sqlwatcher. SqlDependency is used to 'pulse' a query, of which there can only ever be one running at a time.

--- src/Cedar.EventStore.MsSql2008.Tests/ExploratoryTests.cs
+++ src/Cedar.EventStore.MsSql2008.Tests/ExploratoryTests.cs
@@ -1,10 +1,10 @@
 ﻿namespace Cedar.EventStore
 {
     using System;
-    using System.Data.SqlClient;
+    using System.Diagnostics;
     using System.Threading.Tasks;
-    using Cedar.EventStore.Infrastructure;
     using Cedar.EventStore.Streams;
+    using Cedar.EventStore.Subscriptions;
     using Xunit;
     using Xunit.Abstractions;
 
@@ -26,98 +26,25 @@
                 {
                     Func<int, int, Task> createStreams = async (count, interval) =>
                     {
+                        var stopwatch = Stopwatch.StartNew();
                         for(int i = 0; i < count; i++)
                         {
                             var newStreamEvent = new NewStreamEvent(Guid.NewGuid(), "MyEventType", "{}");
-                            await eventStore.AppendToStream($"stream-{i}", ExpectedVersion.NoStream, newStreamEvent);
-
-                            await Task.Delay(interval);
+                            await eventStore.AppendToStream($"stream-{i}-1", ExpectedVersion.NoStream, newStreamEvent);
                         }
+
+                        _testOutputHelper.WriteLine(stopwatch.ElapsedMilliseconds.ToString());
                     };
 
-
-                    using (var watcher = new SqlWatcher(fixture.ConnectionString, _testOutputHelper, eventStore))
+                    using (var watcher = new SqlWatcher(fixture.ConnectionString, eventStore))
                     {
                         await watcher.Start();
 
-                        await createStreams(250, 1);
+                        await createStreams(500, 1);
 
-                        await Task.Delay(2000);
-
-                        _testOutputHelper.WriteLine(watcher._totalCount.ToString());
+                        await Task.Delay(500);
                     }
                 }
-            }
-        }
-
-        private class SqlWatcher : IDisposable
-        {
-            private readonly string _connectionString;
-            private readonly ITestOutputHelper _testOutputHelper;
-            private readonly IEventStore _eventStore;
-            private readonly SqlConnection _connection;
-            private SqlCommand _command;
-            private LongCheckpoint _checkpoint;
-            public int _totalCount;
-
-            public SqlWatcher(string connectionString, ITestOutputHelper testOutputHelper, IEventStore eventStore)
-            {
-                _connectionString = connectionString;
-                _testOutputHelper = testOutputHelper;
-                _eventStore = eventStore;
-                _connection = new SqlConnection(_connectionString);
-                SqlDependency.Start(connectionString);
-            }
-
-            public async Task Start()
-            {
-                var allEventsPage = await _eventStore.ReadAll(_eventStore.EndCheckpoint, 1, ReadDirection.Backward);
-
-                _checkpoint = LongCheckpoint.Parse(allEventsPage.NextCheckpoint);
-
-                await _connection.OpenAsync();
-
-                await GetData();
-            }
-
-            private async Task GetData()
-            {
-                var allEventsPage = await _eventStore.ReadAll(_checkpoint.Value, 1000);
-
-                _testOutputHelper.WriteLine("-----");
-                _testOutputHelper.WriteLine($"From: {allEventsPage.FromCheckpoint}");
-                _testOutputHelper.WriteLine($"To: {allEventsPage.NextCheckpoint}");
-                _testOutputHelper.WriteLine($"Count: {allEventsPage.StreamEvents.Count}");
-                _checkpoint = LongCheckpoint.Parse(allEventsPage.NextCheckpoint);
-
-                _totalCount += allEventsPage.StreamEvents.Count;
-
-                await Task.Delay(100);
-
-                _command = new SqlCommand("SELECT Id FROM dbo.Events", _connection)
-                {
-                    Notification = null
-                };
-                var sqlDependency = new SqlDependency(_command);
-                sqlDependency.OnChange += SqlDependencyOnOnChange;
-
-                await _command.ExecuteNonQueryAsync();
-            }
-
-            private void SqlDependencyOnOnChange(object sender, SqlNotificationEventArgs sqlNotificationEventArgs)
-            {
-                SqlDependency dependency = (SqlDependency)sender;
-                dependency.OnChange -= SqlDependencyOnOnChange;
-
-                Task.Run(GetData);
-            }
-
-            public void Dispose()
-            {
-                _command.Dispose();
-                SqlConnection.ClearPool(_connection);
-                _connection.Dispose();
-                SqlDependency.Stop(_connectionString);
             }
         }
     }
---
+++ src/Cedar.EventStore.MsSql2008/Subscriptions/SqlWatcher.cs
@@ -0,0 +1,90 @@
+﻿namespace Cedar.EventStore.Subscriptions
+{
+    using System;
+    using System.Data.SqlClient;
+    using System.Threading.Tasks;
+    using Cedar.EventStore.Infrastructure;
+    using Cedar.EventStore.Streams;
+
+    public class SqlWatcher : IDisposable
+    {
+        private readonly SqlConnection _connection;
+        private readonly string _connectionString;
+        private readonly IEventStore _eventStore;
+        private readonly InterlockedBoolean _fetchingEvents = new InterlockedBoolean();
+        private SqlCommand _command;
+        private string _checkpoint;
+
+        public SqlWatcher(string connectionString, IEventStore eventStore)
+        {
+            _connectionString = connectionString;
+            _eventStore = eventStore;
+            _connection = new SqlConnection(_connectionString);
+            SqlDependency.Start(connectionString);
+        }
+
+        public void Dispose()
+        {
+            _command.Dispose();
+            SqlConnection.ClearPool(_connection);
+            _connection.Dispose();
+            SqlDependency.Stop(_connectionString);
+        }
+
+        public async Task Start()
+        {
+            var allEventsPage = await _eventStore.ReadAll(_eventStore.EndCheckpoint, 1, ReadDirection.Backward);
+            _checkpoint = allEventsPage.NextCheckpoint;
+
+            await _connection.OpenAsync();
+
+            Subscribe();
+
+            FetchEvents();
+        }
+
+        private void Subscribe()
+        {
+            _command = new SqlCommand("SELECT Id FROM dbo.Events", _connection)
+            {
+                Notification = null
+            };
+            var sqlDependency = new SqlDependency(_command);
+            sqlDependency.OnChange += SqlDependencyOnOnChange;
+
+            _command.ExecuteNonQuery();
+        }
+
+        private void SqlDependencyOnOnChange(object sender, SqlNotificationEventArgs sqlNotificationEventArgs)
+        {
+            var dependency = (SqlDependency) sender;
+            dependency.OnChange -= SqlDependencyOnOnChange;
+
+            Subscribe();
+
+            FetchEvents();
+        }
+
+        private void FetchEvents()
+        {
+            if(_fetchingEvents.EnsureCalledOnce())
+            {
+                return;
+            }
+            Task.Run(async () =>
+            {
+                bool isEnd = false;
+                while(!isEnd)
+                {
+                    var allEventsPage = await _eventStore.ReadAll(_checkpoint, 1000).NotOnCapturedContext();
+                    isEnd = allEventsPage.IsEnd;
+                    _checkpoint = allEventsPage.NextCheckpoint;
+
+                    Console.WriteLine(allEventsPage.NextCheckpoint);
+                }
+
+                _fetchingEvents.Set(false);
+            });
+        }
+    }
+}

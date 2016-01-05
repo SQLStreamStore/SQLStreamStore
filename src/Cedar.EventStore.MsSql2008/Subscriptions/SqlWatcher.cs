2d980631f033da07b6a49aff32a16bf4c5ca05f1
Damian Hickey <dhickey@gmail.com>
2016-01-05 12:51:03 +0100

    Added a polling timer because SqlDependency is not rock solid.

--- src/Cedar.EventStore.MsSql2008/Subscriptions/SqlWatcher.cs
+++ src/Cedar.EventStore.MsSql2008/Subscriptions/SqlWatcher.cs
@@ -6,32 +6,79 @@
     using System.Threading.Tasks;
     using Cedar.EventStore.Infrastructure;
     using Cedar.EventStore.Streams;
+    using Timer = System.Timers.Timer;
 
     public class SqlWatcher : IDisposable
     {
         private readonly SqlConnection _connection;
         private readonly string _connectionString;
         private readonly IEventStore _eventStore;
+        private readonly bool _callSqlDependencyStartStop;
         private readonly InterlockedBoolean _fetchingEvents = new InterlockedBoolean();
         private SqlCommand _command;
         private string _checkpoint;
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private readonly Timer _timer;
 
-        public SqlWatcher(string connectionString, IEventStore eventStore)
+        /// <summary>
+        ///     Initialized a new instance of <see cref="SqlWatcher" />
+        /// </summary>
+        /// <param name="connectionString">
+        ///     The connection string to the event store. Used for monitoring commits with SqlDependency.
+        /// </param>
+        /// <param name="eventStore">
+        ///     An IEventStore instance.
+        /// </param>
+        /// <param name="pollingInterval">
+        ///     SqlDependency is not 100% reliable. Occasionally polling will catch situations where a notification
+        ///     can be missed. See https://connect.microsoft.com/SQLServer/feedback/details/543921/sqldependency-incorrect-behaviour-after-sql-server-restarts
+        /// </param>
+        /// <param name="callSqlDependencyStartStop">
+        ///     <see cref="SqlDependency"/>.Start is a nasty global static thing. As this is a class library the host
+        ///     application may be doing it's own SqlDependency work with this query string. In that case, the host
+        ///     application owns that operation and should call it accordingly. In such cases this paramater should be
+        ///     be set to false. Defaults is true. See http://stackoverflow.com/a/9120471 
+        /// </param>
+        public SqlWatcher(
+            string connectionString,
+            IEventStore eventStore,
+            int pollingInterval = 5000,
+            bool callSqlDependencyStartStop = true)
         {
             _connectionString = connectionString;
             _eventStore = eventStore;
+            _callSqlDependencyStartStop = callSqlDependencyStartStop;
             _connection = new SqlConnection(_connectionString);
-            SqlDependency.Start(connectionString);
+
+            if(callSqlDependencyStartStop)
+            {
+                SqlDependency.Start(connectionString);
+            }
+
+            _timer = new System.Timers.Timer(pollingInterval)
+            {
+                AutoReset = true
+            };
+
+            _timer.Elapsed += (_, __) =>
+            {
+                FetchEvents();
+            };
+            _timer.Start();
         }
 
         public void Dispose()
         {
             _cancellationTokenSource.Cancel();
+            _timer.Dispose();
             _command.Dispose();
             SqlConnection.ClearPool(_connection);
             _connection.Dispose();
-            SqlDependency.Stop(_connectionString);
+
+            if(_callSqlDependencyStartStop)
+            {
+                SqlDependency.Stop(_connectionString);
+            }
         }
 
         public async Task Start()
@@ -81,6 +128,7 @@
             }
             Task.Run(async () =>
             {
+                // TODO exception / error handling
                 bool isEnd = false;
                 while(!isEnd)
                 {

8bc933aa2ea727c426a11a8dc6bcd37592d1cc2f
Damian Hickey <dhickey@gmail.com>
2016-01-24 19:22:53 +0100

    Test to see if it's possible to add duplicate events ids to a stream

--- src/Cedar.EventStore.GetEventStore.Tests/Exploratory.cs
+++ src/Cedar.EventStore.GetEventStore.Tests/Exploratory.cs
@@ -8,25 +8,24 @@ namespace Cedar.EventStore
     using global::EventStore.ClientAPI.Embedded;
     using global::EventStore.ClientAPI.SystemData;
     using global::EventStore.Core;
+    using Shouldly;
     using Xunit;
     using Xunit.Abstractions;
 
-    public class Exploratory
+    public class Exploratory : IDisposable
     {
         private readonly ITestOutputHelper _testOutputHelper;
         private int _eventCounter;
+        private readonly ClusterVNode _node;
+        private readonly ConnectionSettingsBuilder _connectionSettingsBuilder;
 
         public Exploratory(ITestOutputHelper testOutputHelper)
         {
             _testOutputHelper = testOutputHelper;
-        }
 
-        [Fact]
-        public async Task WHat_happens_when_a_stream_is_deleted()
-        {
             IPEndPoint noEndpoint = new IPEndPoint(IPAddress.None, 0);
 
-            ClusterVNode node = EmbeddedVNodeBuilder
+            _node = EmbeddedVNodeBuilder
                 .AsSingleNode()
                 .WithExternalTcpOn(noEndpoint)
                 .WithInternalTcpOn(noEndpoint)
@@ -36,19 +35,23 @@ namespace Cedar.EventStore
                 .WithTfChunkSize(16000000)
                 .RunInMemory();
 
-            await node.StartAndWaitUntilInitialized();
-
-            var connectionSettingsBuilder = ConnectionSettings
+            _connectionSettingsBuilder = ConnectionSettings
                 .Create()
                 .SetDefaultUserCredentials(new UserCredentials("admin", "changeit"))
                 .KeepReconnecting();
+        }
 
-            using(var connection = EmbeddedEventStoreConnection.Create(node, connectionSettingsBuilder))
+        [Fact]
+        public async Task What_happens_when_a_stream_is_deleted()
+        {
+            await _node.StartAndWaitUntilInitialized();
+
+            using(var connection = EmbeddedEventStoreConnection.Create(_node, _connectionSettingsBuilder))
             {
                 using(await connection.SubscribeToStreamAsync("stream-1", true, PrintEvent))
                 {
                     await connection.AppendToStreamAsync("stream-1",
-                        global::EventStore.ClientAPI.ExpectedVersion.Any,
+                        ExpectedVersion.Any,
                         new EventData(Guid.NewGuid(), "event", true, Encoding.UTF8.GetBytes("{}"), null));
 
                     await connection.DeleteStreamAsync("stream-1", global::EventStore.ClientAPI.ExpectedVersion.Any);
@@ -59,7 +62,7 @@ namespace Cedar.EventStore
                 using (await connection.SubscribeToAllAsync(true, PrintEvent))
                 {
                     await connection.AppendToStreamAsync("stream-2",
-                        global::EventStore.ClientAPI.ExpectedVersion.Any,
+                        ExpectedVersion.Any,
                         new EventData(Guid.NewGuid(), "myevent", true, Encoding.UTF8.GetBytes("{}"), null));
 
                     await connection.DeleteStreamAsync("stream-2", global::EventStore.ClientAPI.ExpectedVersion.Any);
@@ -69,6 +72,29 @@ namespace Cedar.EventStore
             }
         }
 
+        [Fact]
+        public async Task Can_append_event_with_duplicate_id_to_stream()
+        {
+            await _node.StartAndWaitUntilInitialized();
+
+            using(var connection = EmbeddedEventStoreConnection.Create(_node, _connectionSettingsBuilder))
+            {
+                string streamId = "stream-1";
+                var eventData = new EventData(Guid.NewGuid(), "type", false, null, null);
+
+                await connection.AppendToStreamAsync(streamId, ExpectedVersion.NoStream, eventData);
+                await connection.AppendToStreamAsync(streamId, 0, eventData);
+
+                var streamEventsSlice = await connection.ReadStreamEventsForwardAsync(streamId, StreamPosition.Start, 2, true);
+
+                streamEventsSlice.Events.Length.ShouldBe(2);
+            }
+        }
+
+        public void Dispose()
+        {
+            _node.Stop();
+        }
 
         private void PrintEvent(EventStoreSubscription eventStoreSubscription, ResolvedEvent resolvedEvent)
         {

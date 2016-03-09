367f4b85031e9db2a864b27b01c04ab24b0c1244
thefringeninja <joao.paul@braganca.name>
2016-03-09 12:05:39 +0100

    additional tests to make sure we can subscribe before anything is written

--- src/Cedar.EventStore.AcceptanceTests/EventStoreAcceptanceTests.Subscriptions.cs
+++ src/Cedar.EventStore.AcceptanceTests/EventStoreAcceptanceTests.Subscriptions.cs
@@ -53,6 +53,50 @@
         }
 
         [Fact]
+        public async Task Can_subscribe_to_a_stream_from_start_before_events_are_written()
+        {
+            using (var fixture = GetFixture())
+            {
+                using (var eventStore = await fixture.GetEventStore())
+                {
+                    string streamId1 = "stream-1";
+
+                    string streamId2 = "stream-2";
+
+                    var done = new TaskCompletionSource<StreamEvent>();
+                    var receivedEvents = new List<StreamEvent>();
+                    using (var subscription = await eventStore.SubscribeToStream(
+                        streamId1,
+                        StreamVersion.Start,
+                        streamEvent =>
+                        {
+                            receivedEvents.Add(streamEvent);
+                            if (streamEvent.StreamVersion == 12)
+                            {
+                                done.SetResult(streamEvent);
+                            }
+                            return Task.CompletedTask;
+                        }))
+                    {
+                        await AppendEvents(eventStore, streamId1, 2);
+
+                        await AppendEvents(eventStore, streamId1, 10);
+
+                        await AppendEvents(eventStore, streamId2, 10);
+
+                        var receivedEvent = await done.Task.WithTimeout();
+
+                        receivedEvents.Count.ShouldBe(12);
+                        subscription.StreamId.ShouldBe(streamId1);
+                        receivedEvent.StreamId.ShouldBe(streamId1);
+                        receivedEvent.StreamVersion.ShouldBe(11);
+                        subscription.LastVersion.ShouldBeGreaterThan(0);
+                    }
+                }
+            }
+        }
+
+        [Fact]
         public async Task Can_subscribe_to_all_stream_from_start()
         {
             using (var fixture = GetFixture())
@@ -80,6 +124,46 @@
                             return Task.CompletedTask;
                         }))
                     {
+                        await AppendEvents(eventStore, streamId1, 1);
+
+                        await receiveEvents.Task.WithTimeout();
+
+                        receivedEvents.Count.ShouldBe(7);
+                    }
+                }
+            }
+        }
+
+        [Fact]
+        public async Task Can_subscribe_to_all_stream_from_start_before_events_are_written()
+        {
+            using (var fixture = GetFixture())
+            {
+                using (var eventStore = await fixture.GetEventStore())
+                {
+                    string streamId1 = "stream-1";
+
+                    string streamId2 = "stream-2";
+
+                    var receiveEvents = new TaskCompletionSource<StreamEvent>();
+                    List<StreamEvent> receivedEvents = new List<StreamEvent>();
+                    using (await eventStore.SubscribeToAll(
+                        null,
+                        streamEvent =>
+                        {
+                            _testOutputHelper.WriteLine($"Received event {streamEvent.StreamId} {streamEvent.StreamVersion} {streamEvent.Checkpoint}");
+                            receivedEvents.Add(streamEvent);
+                            if (streamEvent.StreamId == streamId1 && streamEvent.StreamVersion == 3)
+                            {
+                                receiveEvents.SetResult(streamEvent);
+                            }
+                            return Task.CompletedTask;
+                        }))
+                    {
+                        await AppendEvents(eventStore, streamId1, 3);
+
+                        await AppendEvents(eventStore, streamId2, 3);
+
                         await AppendEvents(eventStore, streamId1, 1);
 
                         await receiveEvents.Task.WithTimeout();

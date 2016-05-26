68d0e2aab5b08bece0b317bf2e906267daf5f50e
Damian Hickey <dhickey@gmail.com>
2016-05-26 17:25:41 +0200

    This test was a bad idea

--- src/Cedar.EventStore.MsSql2008.Tests/MsSqlEventStoreAcceptanceTests.cs
+++ src/Cedar.EventStore.MsSql2008.Tests/MsSqlEventStoreAcceptanceTests.cs
@@ -46,43 +46,5 @@
                 }
             }
         }
-
-        [Fact]
-        public async Task MainAsync()
-        {
-            using (var fixture = new MsSqlEventStoreFixture("dbo"))
-            {
-                using(var eventStore = await fixture.GetEventStore())
-                {
-                    var cts = new CancellationTokenSource();
-                    var tasks = new List<Task>();
-
-                    for(int i = 0; i < Environment.ProcessorCount; i++)
-                    {
-                        var task = Task.Run(async () =>
-                        {
-                            int eventNumber = i*10000000;
-                            while(!cts.IsCancellationRequested)
-                            {
-                                try
-                                {
-                                    await eventStore.AppendToStream("stream-1",
-                                        ExpectedVersion.Any,
-                                        CreateNewStreamEvents(eventNumber, eventNumber + 1),
-                                        cts.Token);
-                                    eventNumber += 2;
-                                }
-                                catch(Exception ex) when(!(ex is TaskCanceledException))
-                                {
-                                    throw;
-                                }
-                            }
-                        }, cts.Token);
-                        tasks.Add(task);
-                    }
-                    await Task.WhenAll(tasks);
-                }
-            }
-        }
     }
 }

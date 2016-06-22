efe533d9e29bd41800408d3bbb5d877b53677832
Damian Hickey <dhickey@gmail.com>
2016-06-22 08:13:57 +0200

    Removes R# warning

--- src/Cedar.EventStore/App_Packages/LibLog.4.2/LibLog.cs
+++ src/Cedar.EventStore/App_Packages/LibLog.4.2/LibLog.cs
@@ -742,7 +742,6 @@ namespace Cedar.EventStore.Logging.LogProviders
     using System.Linq.Expressions;
     using System.Reflection;
 #if !LIBLOG_PORTABLE
-    using System.Text;
 #endif
     using System.Text.RegularExpressions;
 

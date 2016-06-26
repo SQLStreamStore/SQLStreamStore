6767263d3490f0280192264957787a6a90acec76
Damian Hickey <dhickey@gmail.com>
2016-06-27 00:40:17 +0200

    Some comments around stream deleting.

--- src/Cedar.EventStore/IEventStore.cs
+++ src/Cedar.EventStore/IEventStore.cs
@@ -12,6 +12,15 @@
             NewStreamEvent[] events,
             CancellationToken cancellationToken = default(CancellationToken));
 
+        /// <summary>
+        ///     Hard deletes a stream and all of its events. Deleting a stream will result in a '$stream-deleted'
+        ///     event being appended to the '$deleted' stream.
+        /// </summary>
+        /// <param name="streamId">The stream Id to delete</param>
+        /// <param name="expectedVersion">The stream expected version. If it does not match, a
+        ///     <see cref="WrongExpectedVersionException"/> will be thrown.</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the operations.</param>
+        /// <returns></returns>
         Task DeleteStream(
             string streamId,
             int expectedVersion = ExpectedVersion.Any,

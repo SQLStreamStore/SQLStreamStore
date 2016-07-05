5fdf0297d4908ca20407cd1ee9376eca2aad98b1
Damian Hickey <dhickey@gmail.com>
2016-07-05 18:02:44 +0200

    Start of Scavenger project using SqlLite for local projections.

---
+++ src/Cedar.EventStore.Scavenging/Scavenger.cs
@@ -0,0 +1,66 @@
+﻿namespace Cedar.EventStore.Scavenging
+{
+    using System;
+    using System.Data.SQLite;
+    using System.IO;
+    using System.Linq;
+
+    public class Scavenger : IDisposable
+    {
+        private SQLiteConnection _connection;
+
+        public Scavenger(IEventStore eventStore, string path)
+        {
+            string dbPath = Path.Combine(path, "scavenger.db");
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            if(!File.Exists(dbPath))
+            {
+                var assembly = typeof(Scavenger).Assembly;
+                var resourceName = assembly
+                    .GetManifestResourceNames()
+                    .Single(name => name.EndsWith("scavenger.sqlite"));
+                using(var file = File.OpenWrite(dbPath))
+                {
+                    using(Stream stream = assembly.GetManifestResourceStream(resourceName))
+                    {
+                        stream.CopyTo(file);
+                    }
+                }
+            }
+
+            _connection = new SQLiteConnection($"Data Source={dbPath};Version=3;");
+        }
+
+        public void Blah()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            _connection?.Dispose();
+            _connection = null;
+        }
+    }
+
+    internal class StreamMetadata
+    {
+        public string StreamId { get; set; }
+
+        public int MaxAge { get; set; }
+    }
+
+    internal class StreamEventMaxAge
+    {
+        public string StreamId { get; set; }
+
+        public string EventId { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Expires { get; set; }
+    }
+}
---
+++ src/Cedar.EventStore.Tests/Scavenging/ScavengerTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Cedar.EventStore.Scavenging
+{
+    using System;
+    using Xunit;
+
+    public class ScavengerTests
+    {
+        [Fact]
+        public void Blah()
+        {
+            using(var eventStore = new InMemoryEventStore())
+            {
+                using(var scavenger = new Scavenger(eventStore, $"scavenger/{Guid.NewGuid()}"))
+                {
+                    scavenger.Blah();
+                }
+            }
+        }
+    }
+}

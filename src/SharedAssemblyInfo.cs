b4e49be0e3ba60bfb6b53f28a92726684990bb41
Damian Hickey <dhickey@gmail.com>
2016-09-13 15:35:37 +0200

    Actually make this 0.2.0 because of existing pre-release packages on CI feed.

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyProduct("SqlStreamStore")]
 [assembly: AssemblyCopyright("Copyright Damian Hickey & Contributors 2015 - 2016")]
-[assembly: AssemblyVersion("0.1.2.0")]
-[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]

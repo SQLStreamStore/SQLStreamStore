88c427510ab705b5f82725275aea5865d0ec9eaa
Damian Hickey <dhickey@gmail.com>
2016-10-03 09:44:36 +0200

    Version 0.4.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyProduct("SqlStreamStore")]
 [assembly: AssemblyCopyright("Copyright Damian Hickey & Contributors 2015 - 2016")]
-[assembly: AssemblyVersion("0.3.2.0")]
-[assembly: AssemblyFileVersion("0.3.2.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]

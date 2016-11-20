542ae574967577966d7cb23bc83fc855592fcdbe
Richard Bennett <richard@adaptpos.com>
2016-11-20 07:49:20 -0500

    Initial project setup.

---
+++ src/SqlStreamStore.SQLite/SQLiteStreamStoreSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using EnsureThat;
+
+using SqlStreamStore.Infrastructure;
+using SqlStreamStore.Subscriptions;
+
+namespace SqlStreamStore.SQLite {
+	public class SQLiteStreamStoreSettings {
+		public string ConnectionString { get; private set; }
+
+		public CreateStreamStoreNotifier CreateStreamStoreNotifier { get; set; } = store => new PollingStreamStoreNotifier(store);
+
+		public TimeSpan MetadataMaxAgeCacheExpire { get; set; } = TimeSpan.FromMinutes(1);
+
+		public int MetadataMaxAgeCacheMaxSize { get; set; } = 10000;
+
+		public GetUtcNow GetUtcNow { get; set; }
+
+		public string LogName { get { return "SQLiteStreamStore"; } }
+
+		public SQLiteStreamStoreSettings(string connectionString) {
+			Ensure.That(connectionString, nameof(connectionString)).IsNotNullOrWhiteSpace();
+
+			ConnectionString = connectionString;
+		}
+	}
+}

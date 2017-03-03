0a37a679538db61ae99afd40dae8b37faad796e5
Damian Hickey <dhickey@gmail.com>
2017-03-03 18:34:13 +0100

    Examples are now runnable.

---
+++ src/Example/Example.cs
@@ -0,0 +1,31 @@
+﻿namespace Example
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using EasyConsole;
+    using SqlStreamStore;
+
+    public abstract class Example
+    {
+        public void Run(CancellationToken cancellationToken)
+        {
+            Task.Run(() => RunAsync(cancellationToken)).GetAwaiter().GetResult();
+        }
+
+        protected abstract Task RunAsync(CancellationToken cancellationToken);
+
+        protected IStreamStore GetStore()
+        {
+            IStreamStore streamStore = null;
+
+            Output.WriteLine(ConsoleColor.Yellow, "Store type:");
+            new Menu()
+                .Add("InMem", () => streamStore = new InMemoryStreamStore())
+                .Add("MS SQL (SqlLocalDB, NamedPipes)", () => streamStore = new MsSqlStreamStoreFixture("dbo").GetStreamStore().Result)
+                .Display();
+
+            return streamStore;
+        }
+    }
+}
---
+++ src/Example/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Example
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using EasyConsole;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, __) => cts.Cancel();
+
+            Output.WriteLine(ConsoleColor.Yellow, "Choose a test:");
+            new Menu()
+                .Add(
+                    "Subscribe to all",
+                    () => new SubscribeToAll().Run(cts.Token))
+/*                .Add(
+                    "Read All",
+                    () => new ReadAll().Run(cts.Token))
+                .Add(
+                    "Append Max Count",
+                    () => new AppendMaxCount().Run(cts.Token))*/
+                .Display();
+
+            if(Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}

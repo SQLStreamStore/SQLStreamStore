9d4e87988146877709526405e83fc4533e583306
Yves Reynhout <yves.reynhout@gmail.com>
2017-03-21 10:36:48 +0100

    Added a failing test for the code I removed.

--- src/SqlStreamStore/Streams/AppendResult.cs
+++ src/SqlStreamStore/Streams/AppendResult.cs
@@ -5,16 +5,14 @@
     /// </summary>
     public class AppendResult
     {
-        /// <summary>The current version the stream that was affected by the append operation is at.</summary>
+        /// <summary>
+        ///     The current version the stream that was affected by the append operation is at.
+        /// </summary>
         public readonly int CurrentVersion;
-        /// <summary>The current position the stream that was affected by the append operation is at.</summary>
+        /// <summary>
+        ///     The current position the stream that was affected by the append operation is at.
+        /// </summary>
         public readonly long CurrentPosition;
-
-        public AppendResult(int currentVersion)
-        {
-            CurrentVersion = currentVersion;
-            CurrentPosition = -1;
-        }
 
         public AppendResult(int currentVersion, long currentPosition)
         {

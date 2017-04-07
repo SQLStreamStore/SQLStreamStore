4c6f11e64b1cc7a2c75079a535b3e516793cac7d
Damian Hickey <dhickey@gmail.com>
2017-04-07 13:13:08 +0200

    Migration script to update an existing store to new schema.

---
+++ src/SqlStreamStore.MsSql/MsSqlScripts/Migration_v2.sql
@@ -0,0 +1,21 @@
+/*** Make sure you back up your database first! ***/
+
+BEGIN
+    ALTER TABLE [dbo].[Streams]
+    ADD [Position] bigint NOT NULL DEFAULT 0;
+END
+
+BEGIN
+    UPDATE [dbo].[Streams]
+    SET [dbo].[Streams].[Position] = (SELECT MAX([dbo].[Messages].[Position])
+      FROM [dbo].[Messages]
+      WHERE [dbo].[Messages].[StreamIdInternal] = [dbo].[Streams].[IdInternal])
+END
+
+BEGIN
+    EXEC sys.sp_addextendedproperty   
+    @name = N'version',
+    @value = N'2',
+    @level0type = N'SCHEMA', @level0name = 'dbo',
+    @level1type = N'TABLE',  @level1name = 'Streams';
+END

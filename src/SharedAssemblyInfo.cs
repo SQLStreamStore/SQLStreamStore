9266086bb51a40a1be89b9d4377df4cab73494ff
Damian Hickey <dhickey@gmail.com>
2017-04-07 14:44:21 +0200

    Set version to 0.8.0

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("SqlStreamStore")]
 [assembly: AssemblyCopyright("Copyright Damian Hickey & Contributors 2015 - 2017")]
-[assembly: System.Reflection.AssemblyVersion("0.6.0")]
-[assembly: System.Reflection.AssemblyFileVersion("0.6.0")]
+[assembly: System.Reflection.AssemblyVersion("0.8.0")]
+[assembly: System.Reflection.AssemblyFileVersion("0.8.0")]

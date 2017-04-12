0f9788ed73d9e6c8a2ca6b25cb75f90374787e36
Bennie Copeland <bennie.r.copeland@gmail.com>
2017-04-12 12:12:50 +0200

    Fix versioning of assemblies and nuget packages

--- src/SharedAssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyProduct("SqlStreamStore")]
-[assembly: AssemblyCopyright("Copyright Damian Hickey & Contributors 2015 - 2017")]
-[assembly: System.Reflection.AssemblyVersion("0.8.1")]
-[assembly: System.Reflection.AssemblyFileVersion("0.8.1")]

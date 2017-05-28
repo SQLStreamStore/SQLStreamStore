15c7d796abae8ecc42321c57bafb1d910611693f
Damian Hickey <dhickey@gmail.com>
2017-05-28 22:31:10 +0200

    Unused

--- src/SqlStreamStore/Infrastructure/TaskHelpers.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SqlStreamStore.Infrastructure
-{
-    using System.Threading.Tasks;
-
-    public static class TaskHelpers
-    {
-        public static readonly Task CompletedTask = Task.FromResult(0);
-    }
-}

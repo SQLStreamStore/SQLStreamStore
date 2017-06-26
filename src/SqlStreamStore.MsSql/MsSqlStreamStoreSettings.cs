74efa4f95085030ad0931a58cebbf820bff175c7
Damian Hickey <dhickey@gmail.com>
2017-06-26 15:42:05 +0200

    Extraneous commas

--- src/SqlStreamStore.MsSql/MsSqlStreamStoreSettings.cs
+++ src/SqlStreamStore.MsSql/MsSqlStreamStoreSettings.cs
@@ -53,16 +53,15 @@ namespace SqlStreamStore
         }
 
         /// <summary>
-        ///     To help with perf, the max age of messages in a stream,
+        ///     To help with perf, the max age of messages in a stream
         ///     are cached. It is not expected that a streams max age
         ///     metadata to be changed frequently. Here we hold on to the
         ///     max age for the specified timespan. The default is 1 minute.
         /// </summary>
         public TimeSpan MetadataMaxAgeCacheExpire { get; set; } = TimeSpan.FromMinutes(1);
 
-
         /// <summary>
-        ///     To help with perf, the max age of messages in a stream,
+        ///     To help with perf, the max age of messages in a stream
         ///     are cached. It is not expected that a streams max age
         ///     metadata to be changed frequently. Here we define how many
         ///     items are cached. The default value is 10000.

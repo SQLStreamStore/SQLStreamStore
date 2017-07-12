8efe6e47e7c246c6def8721a6541eb20461f2873
thefringeninja <joao.paul@braganca.name>
2017-07-12 16:25:49 +0200

    return version 0 if streams table does not exist

--- src/SqlStreamStore.MsSql/MsSqlScripts/GetSchemaVersion.sql
+++ src/SqlStreamStore.MsSql/MsSqlScripts/GetSchemaVersion.sql
@@ -1,2 +1,8 @@
+IF OBJECT_ID('dbo.Streams', 'U') IS NULL
+BEGIN
+    SELECT 'version' as name, '0' as value
+    RETURN
+END
+
 SELECT name, value
 FROM fn_listextendedproperty (NULL, 'schema', 'dbo', 'table', default, NULL, NULL);

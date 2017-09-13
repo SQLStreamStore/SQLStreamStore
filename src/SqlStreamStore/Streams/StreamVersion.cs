57941e4435ec44d73d4f0ac25569ab6ccebdb77c
Damian Hickey <dhickey@gmail.com>
2017-09-13 13:56:13 +0200

    Add Position.None and StreamVersion.None constants (actually static readonly) for use in subscriptions instead of using 'null'.
    Updated tests to use these.

--- src/SqlStreamStore/Streams/StreamVersion.cs
+++ src/SqlStreamStore/Streams/StreamVersion.cs
@@ -6,12 +6,17 @@
     public static class StreamVersion
     {
         /// <summary>
-        /// The first message in a stream
+        ///     No stream version.
+        /// </summary>
+        public static readonly int? None = null;
+
+        /// <summary>
+        ///     The first message in a stream
         /// </summary>
         public const int Start = 0;
 
         /// <summary>
-        /// The last message in the stream.
+        ///     The last message in the stream.
         /// </summary>
         public const int End = -1;
     }

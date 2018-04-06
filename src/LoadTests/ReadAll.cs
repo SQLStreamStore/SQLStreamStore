6ff2a7999bc230ecd55df7028cdef19eb05996f1
thefringeninja <joao.paul@braganca.name>
2018-04-06 15:02:53 +0200

    disposing the fixture may matter depending on the implementation

--- src/LoadTests/ReadAll.cs
+++ src/LoadTests/ReadAll.cs
@@ -15,31 +15,41 @@
             Output.WriteLine(ConsoleColor.Green, "Appends events to streams and reads them all back in a single task.");
             Output.WriteLine("");
 
-            var streamStore = GetStore();
+            var (streamStore, dispose) = GetStore();
 
-            await new UniqueStreams()
-                .Append(streamStore, ct);
-
-            int readPageSize = Input.ReadInt("Read page size: ", 1, 10000);
-            var prefectch = Input.ReadEnum<YesNo>("Prefetch: ");
-
-            var stopwatch = Stopwatch.StartNew();
-            int count = 0;
-            var position = Position.Start;
-            ReadAllPage page;
-            do
+            try
             {
-                page = await streamStore.ReadAllForwards(position, readPageSize, prefetchJsonData: prefectch == YesNo.Yes, cancellationToken: ct);
-                count += page.Messages.Length;
-                Console.Write($"\r> Read {count}");
-                position = page.NextPosition;
-            } while (!page.IsEnd);
+                await new UniqueStreams()
+                    .Append(streamStore, ct);
 
-            stopwatch.Stop();
-            var rate = Math.Round((decimal)count / stopwatch.ElapsedMilliseconds * 1000, 0);
+                int readPageSize = Input.ReadInt("Read page size: ", 1, 10000);
+                var prefectch = Input.ReadEnum<YesNo>("Prefetch: ");
 
-            Output.WriteLine("");
-            Output.WriteLine($"> {count} messages read in {stopwatch.Elapsed} ({rate} m/s)");
+                var stopwatch = Stopwatch.StartNew();
+                int count = 0;
+                var position = Position.Start;
+                ReadAllPage page;
+                do
+                {
+                    page = await streamStore.ReadAllForwards(position,
+                        readPageSize,
+                        prefetchJsonData: prefectch == YesNo.Yes,
+                        cancellationToken: ct);
+                    count += page.Messages.Length;
+                    Console.Write($"\r> Read {count}");
+                    position = page.NextPosition;
+                } while(!page.IsEnd);
+
+                stopwatch.Stop();
+                var rate = Math.Round((decimal) count / stopwatch.ElapsedMilliseconds * 1000, 0);
+
+                Output.WriteLine("");
+                Output.WriteLine($"> {count} messages read in {stopwatch.Elapsed} ({rate} m/s)");
+            }
+            finally
+            {
+                dispose();
+            }
         }
 
         private enum YesNo

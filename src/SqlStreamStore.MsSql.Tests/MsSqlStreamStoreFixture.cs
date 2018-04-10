a925cd0d689b15d5f0bce1c3b6f0786f612072d5
thefringeninja <joao.paul@braganca.name>
2018-04-10 13:45:11 +0200

    using a docker container to test mssql if we're not on windows

--- src/SqlStreamStore.MsSql.Tests/MsSqlStreamStoreFixture.cs
+++ src/SqlStreamStore.MsSql.Tests/MsSqlStreamStoreFixture.cs
@@ -2,6 +2,7 @@ namespace SqlStreamStore
 {
     using System;
     using System.Data.SqlClient;
+    using System.Threading;
     using System.Threading.Tasks;
     using SqlStreamStore.Infrastructure;
 
@@ -10,15 +11,15 @@ namespace SqlStreamStore
         public readonly string ConnectionString;
         private readonly string _schema;
         private readonly string _databaseName;
-        private readonly ILocalInstance _localInstance;
+        private readonly ISqlServerDatabase _localInstance;
 
         public MsSqlStreamStoreFixture(string schema)
         {
             _schema = schema;
-            _localInstance = new LocalInstance();
-
-            var uniqueName = Guid.NewGuid().ToString().Replace("-", string.Empty);
-            _databaseName = $"StreamStoreTests-{uniqueName}";
+            _databaseName = $"StreamStoreTests-{Guid.NewGuid():n}";
+            _localInstance = Environment.OSVersion.IsWindows()
+                ? (ISqlServerDatabase) new LocalSqlServerDatabase(_databaseName)
+                : new DockerSqlServerDatabase(_databaseName);
 
             ConnectionString = CreateConnectionString();
         }
@@ -108,49 +109,118 @@ namespace SqlStreamStore
             }
         }
 
-        private async Task CreateDatabase()
-        {
-            using(var connection = _localInstance.CreateConnection())
-            {
-                await connection.OpenAsync().NotOnCapturedContext();
-                var tempPath = Environment.GetEnvironmentVariable("Temp");
-                var createDatabase = $"CREATE DATABASE [{_databaseName}] on (name='{_databaseName}', "
-                                     + $"filename='{tempPath}\\{_databaseName}.mdf')";
-                using (var command = new SqlCommand(createDatabase, connection))
-                {
-                    await command.ExecuteNonQueryAsync();
-                }
-            }
-        }
+        private Task CreateDatabase() => _localInstance.CreateDatabase();
 
         private string CreateConnectionString()
         {
             var connectionStringBuilder = _localInstance.CreateConnectionStringBuilder();
             connectionStringBuilder.MultipleActiveResultSets = true;
-            connectionStringBuilder.IntegratedSecurity = true;
             connectionStringBuilder.InitialCatalog = _databaseName;
 
             return connectionStringBuilder.ToString();
         }
 
-        private interface ILocalInstance
+        private interface ISqlServerDatabase
         {
             SqlConnection CreateConnection();
             SqlConnectionStringBuilder CreateConnectionStringBuilder();
+            Task CreateDatabase(CancellationToken cancellationToken = default(CancellationToken));
         }
 
-        private class LocalInstance : ILocalInstance
+        private class LocalSqlServerDatabase : ISqlServerDatabase
         {
-            private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=SSPI;";
+            private readonly string _databaseName;
+            private readonly string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=SSPI;";
 
+            public LocalSqlServerDatabase(string databaseName)
+            {
+                _databaseName = databaseName;
+            }
             public SqlConnection CreateConnection()
             {
-                return new SqlConnection(connectionString);
+                return new SqlConnection(_connectionString);
             }
 
             public SqlConnectionStringBuilder CreateConnectionStringBuilder()
             {
-                return new SqlConnectionStringBuilder(connectionString);
+                return new SqlConnectionStringBuilder(_connectionString);
+            }
+
+            public async Task CreateDatabase(CancellationToken cancellationToken = default(CancellationToken))
+            {
+                using(var connection = CreateConnection())
+                {
+                    await connection.OpenAsync(cancellationToken).NotOnCapturedContext();
+                    var tempPath = Environment.GetEnvironmentVariable("Temp");
+                    var createDatabase = $"CREATE DATABASE [{_databaseName}] on (name='{_databaseName}', "
+                                         + $"filename='{tempPath}\\{_databaseName}.mdf')";
+                    using (var command = new SqlCommand(createDatabase, connection))
+                    {
+                        await command.ExecuteNonQueryAsync(cancellationToken).NotOnCapturedContext();
+                    }
+                }
+            }
+        }
+
+        private class DockerSqlServerDatabase : ISqlServerDatabase
+        {
+            private readonly string _databaseName;
+            private readonly DockerContainer _sqlServerContainer;
+            private readonly string _password;
+            private const string Image = "microsoft/mssql-server-linux";
+            private const string Tag = "2017-CU5";
+            private const int Port = 1433;
+
+            public DockerSqlServerDatabase(string databaseName)
+            {
+                _databaseName = databaseName;
+                _password = "!01u0Yx19PW";
+
+                _sqlServerContainer = new DockerContainer(
+                    Image,
+                    Tag,
+                    HealthCheck,
+                    Port)
+                {
+                    ContainerName = "sql-stream-store-tests-mssql",
+                    Env = new[] { "ACCEPT_EULA=Y", $"SA_PASSWORD={_password}" }
+                };
+            }
+
+            public SqlConnection CreateConnection()
+                => new SqlConnection(CreateConnectionStringBuilder().ConnectionString);
+
+            public SqlConnectionStringBuilder CreateConnectionStringBuilder()
+                => new SqlConnectionStringBuilder($"server=localhost,{Port};User Id=sa;Password={_password};Initial Catalog=master");
+
+            public async Task CreateDatabase(CancellationToken cancellationToken = default(CancellationToken))
+            {
+                await _sqlServerContainer.TryStart(cancellationToken).WithTimeout(3 * 60 * 1000);
+
+                using(var connection = CreateConnection())
+                {
+                    await connection.OpenAsync(cancellationToken).NotOnCapturedContext();
+                    using (var command = new SqlCommand($"CREATE DATABASE [{_databaseName}]", connection))
+                    {
+                        await command.ExecuteNonQueryAsync(cancellationToken).NotOnCapturedContext();
+                    }
+                }
+            }
+
+            private async Task<bool> HealthCheck(CancellationToken cancellationToken)
+            {
+                try
+                {
+                    using(var connection = CreateConnection())
+                    {
+                        await connection.OpenAsync(cancellationToken).NotOnCapturedContext();
+
+                        return true;
+                    }
+                }
+                catch (Exception ex){}
+
+                return false;
             }
         }
     }

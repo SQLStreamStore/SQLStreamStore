4bffa05707ca7518b8be53f32828b81792cf5781
Yves Reynhout <yves.reynhout@gmail.com>
2018-04-12 18:45:28 +0200

    Applied similar changes to ExpectedAny and ExpectedNoStream sql scripts. Applied locks to ExpectedVersion script.

--- src/SqlStreamStore.MsSql/MsSqlScripts/AppendStreamExpectedVersion.sql
+++ src/SqlStreamStore.MsSql/MsSqlScripts/AppendStreamExpectedVersion.sql
@@ -1,40 +1,41 @@
 BEGIN TRANSACTION AppendStream;
-DECLARE @streamIdInternal AS INT;
-DECLARE @latestStreamVersion AS INT;
-DECLARE @latestStreamPosition AS BIGINT;
 
-SELECT @streamIdInternal = dbo.Streams.IdInternal, @latestStreamVersion = dbo.Streams.[Version]
-FROM dbo.Streams
-WHERE dbo.Streams.Id = @streamId;
+    DECLARE @streamIdInternal AS INT;
+    DECLARE @latestStreamVersion AS INT;
+    DECLARE @latestStreamPosition AS BIGINT;
 
-IF @streamIdInternal IS NULL
-    BEGIN
-        ROLLBACK TRANSACTION AppendStream;
-        RAISERROR('WrongExpectedVersion', 16, 1);
-        RETURN;
-    END
-IF @latestStreamVersion != @expectedStreamVersion
-    BEGIN
-        ROLLBACK TRANSACTION AppendStream;
-        RAISERROR('WrongExpectedVersion', 16, 2);
-        RETURN;
-    END
+    SELECT @streamIdInternal = dbo.Streams.IdInternal, @latestStreamVersion = dbo.Streams.[Version]
+    FROM dbo.Streams WITH (UPDLOCK, ROWLOCK)
+    WHERE dbo.Streams.Id = @streamId;
 
-INSERT INTO dbo.Messages
-    (StreamIdInternal, StreamVersion, Id, Created, [Type], JsonData, JsonMetadata)
-    SELECT @streamIdInternal, StreamVersion + @latestStreamVersion + 1, Id, Created, [Type], JsonData, JsonMetadata
+    IF @streamIdInternal IS NULL
+        BEGIN
+            ROLLBACK TRANSACTION AppendStream;
+            RAISERROR('WrongExpectedVersion', 16, 1);
+            RETURN;
+        END
+    IF @latestStreamVersion != @expectedStreamVersion
+        BEGIN
+            ROLLBACK TRANSACTION AppendStream;
+            RAISERROR('WrongExpectedVersion', 16, 2);
+            RETURN;
+        END
+
+    INSERT INTO dbo.Messages
+        (StreamIdInternal, StreamVersion, Id, Created, [Type], JsonData, JsonMetadata)
+        SELECT @streamIdInternal, StreamVersion + @latestStreamVersion + 1, Id, Created, [Type], JsonData, JsonMetadata
+        FROM @newMessages
+        ORDER BY StreamVersion;
+
+    SET @latestStreamPosition = SCOPE_IDENTITY()
+
+    SELECT @latestStreamVersion = MAX(StreamVersion) + @latestStreamVersion + 1
     FROM @newMessages
-    ORDER BY StreamVersion;
 
-SET @latestStreamPosition = SCOPE_IDENTITY()
-
-SELECT @latestStreamVersion = MAX(StreamVersion) + @latestStreamVersion + 1
-FROM @newMessages
-
-UPDATE dbo.Streams
-    SET dbo.Streams.[Version] = @latestStreamVersion,
-        dbo.Streams.[Position] = @latestStreamPosition
-    WHERE dbo.Streams.IdInternal = @streamIdInternal
+    UPDATE dbo.Streams
+        SET dbo.Streams.[Version] = @latestStreamVersion,
+            dbo.Streams.[Position] = @latestStreamPosition
+        WHERE dbo.Streams.IdInternal = @streamIdInternal
 
 COMMIT TRANSACTION AppendStream;
 
--- src/SqlStreamStore.MsSql/MsSqlScripts/AppendStreamExpectedVersionNoStream.sql
+++ src/SqlStreamStore.MsSql/MsSqlScripts/AppendStreamExpectedVersionNoStream.sql
@@ -1,58 +1,50 @@
 BEGIN TRANSACTION CreateStream;
+
     DECLARE @streamIdInternal AS INT;
     DECLARE @latestStreamVersion AS INT;
-	DECLARE @latestStreamPosition AS BIGINT;
+    DECLARE @latestStreamPosition AS BIGINT;
 
     BEGIN
-        INSERT INTO dbo.Streams (Id, IdOriginal) VALUES (@streamId, @streamIdOriginal);
-        SELECT @streamIdInternal = SCOPE_IDENTITY();
 
-            INSERT INTO dbo.Messages (StreamIdInternal, StreamVersion, Id, Created, [Type], JsonData, JsonMetadata)
-                 SELECT @streamIdInternal,
-                        StreamVersion,
-                        Id,
-                        Created,
-                        [Type],
-                        JsonData,
-                        JsonMetadata
-                   FROM @newMessages
-               ORDER BY StreamVersion;
+        INSERT INTO dbo.Streams
+            (Id, IdOriginal)
+        VALUES
+            (@streamId, @streamIdOriginal);
 
-             SELECT TOP(1)
-                    @latestStreamVersion = dbo.Messages.StreamVersion,
-                    @latestStreamPosition = dbo.Messages.Position
-              FROM dbo.Messages
-             WHERE dbo.Messages.StreamIDInternal = @streamIdInternal
-          ORDER BY dbo.Messages.Position DESC
+        SET @streamIdInternal = SCOPE_IDENTITY();
 
-             IF @latestStreamVersion IS NULL
-            SET @latestStreamVersion = -1
+        INSERT INTO dbo.Messages
+            (StreamIdInternal, StreamVersion, Id, Created, [Type], JsonData, JsonMetadata)
+            SELECT @streamIdInternal, StreamVersion, Id, Created, [Type], JsonData, JsonMetadata
+            FROM @newMessages
+            ORDER BY StreamVersion;
 
-             IF @latestStreamPosition IS NULL
-            SET @latestStreamPosition = -1
+        SET @latestStreamPosition = ISNULL(SCOPE_IDENTITY(), -1)
 
-            UPDATE dbo.Streams
-               SET dbo.Streams.[Version] = @latestStreamVersion,
-                   dbo.Streams.[Position] = @latestStreamPosition
-             WHERE dbo.Streams.IdInternal = @streamIdInternal
+        SELECT @latestStreamVersion = MAX(StreamVersion) + @latestStreamVersion + 1
+        FROM @newMessages
+
+        SET @latestStreamVersion = ISNULL(@latestStreamVersion, -1)
+
+        UPDATE dbo.Streams
+            SET dbo.Streams.[Version] = @latestStreamVersion,
+                dbo.Streams.[Position] = @latestStreamPosition
+            WHERE dbo.Streams.IdInternal = @streamIdInternal
+
     END;
+
 COMMIT TRANSACTION CreateStream;
 
 /* Select CurrentVersion, CurrentPosition */
 
-     SELECT currentVersion = @latestStreamVersion, currentPosition = @latestStreamPosition
+SELECT currentVersion = @latestStreamVersion, currentPosition = @latestStreamPosition
 
 /* Select Metadata */
-    DECLARE @metadataStreamId as NVARCHAR(42)
-    DECLARE @metadataStreamIdInternal as INT
-        SET @metadataStreamId = '$$' + @streamId
 
-     SELECT @metadataStreamIdInternal = dbo.Streams.IdInternal
-       FROM dbo.Streams
-      WHERE dbo.Streams.Id = @metadataStreamId;
-
-     SELECT TOP(1)
-            dbo.Messages.JsonData
-       FROM dbo.Messages
-      WHERE dbo.Messages.StreamIdInternal = @metadataStreamIdInternal
-   ORDER BY dbo.Messages.Position DESC;
+SELECT dbo.Messages.JsonData
+FROM dbo.Messages
+WHERE dbo.Messages.Position = (
+    SELECT dbo.Streams.Position
+    FROM dbo.Streams
+    WHERE dbo.Streams.Id = '$$' + @streamId
+)

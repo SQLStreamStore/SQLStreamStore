070147eabd643e521c513978f90eab9675a1d6e4
Damian Hickey <dhickey@gmail.com>
2018-05-20 19:53:38 +0200

    Add a deterministic guid generator and tests

---
+++ src/SqlStreamStore.Tests/Infrastructure/DeterministicGuidGeneratorTests.cs
@@ -0,0 +1,29 @@
+﻿namespace SqlStreamStore.Infrastructure
+{
+    using System;
+    using Shouldly;
+    using Xunit;
+
+    public class DeterministicGuidGeneratorTests
+    {
+        [Fact]
+        public void Given_same_input_should_generate_same_Guid()
+        {
+            var generator = new DeterministicGuidGenerator(Guid.NewGuid());
+            var guid1 = generator.Create("some-data");
+            var guid2 = generator.Create("some-data");
+
+            guid2.ShouldBe(guid1);
+        }
+
+        [Fact]
+        public void Given_different_input_should_generate_different_Guid()
+        {
+            var generator = new DeterministicGuidGenerator(Guid.NewGuid());
+            var guid1 = generator.Create("some-data");
+            var guid2 = generator.Create("other-data");
+
+            guid2.ShouldNotBe(guid1);
+        }
+    }
+}

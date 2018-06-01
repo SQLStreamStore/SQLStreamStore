79af1584bac30b72c24a3297f02af5564e1f73d7
João Bragança <joao.paul@braganca.name>
2018-06-01 15:07:45 +0200

    syntax improvements

--- src/SqlStreamStore.Postgres/PostgresqlStreamId.cs
+++ src/SqlStreamStore.Postgres/PostgresqlStreamId.cs
@@ -11,23 +11,19 @@
 
         public PostgresqlStreamId(string idOriginal)
         {
-            string id;
-            Guid _;
-            if(Guid.TryParse(idOriginal, out _))
-            {
-                id = idOriginal; //If the ID is a GUID, don't bother hashing it.
-            }
-            else
-            {
-                using(var sha1 = SHA1.Create())
-                {
-                    var hashBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(idOriginal));
-                    id = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
-                }
-            }
-
-            Id = id;
+            Id = Guid.TryParse(idOriginal, out _)
+                ? idOriginal
+                : ComputeHash(idOriginal);
             IdOriginal = idOriginal;
+        }
+
+        private static string ComputeHash(string idOriginal)
+        {
+            using(var sha1 = SHA1.Create())
+            {
+                var hashBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(idOriginal));
+                return BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+            }
         }
 
         public static readonly PostgresqlStreamId Deleted
@@ -35,8 +31,10 @@
 
         public static bool operator ==(PostgresqlStreamId left, PostgresqlStreamId right) => left.Equals(right);
         public static bool operator !=(PostgresqlStreamId left, PostgresqlStreamId right) => !left.Equals(right);
-        public bool Equals(PostgresqlStreamId other) 
+
+        public bool Equals(PostgresqlStreamId other)
             => string.Equals(Id, other.Id) && string.Equals(IdOriginal, other.IdOriginal);
+
         public override bool Equals(object obj) => obj is PostgresqlStreamId other && Equals(other);
 
         public override int GetHashCode()

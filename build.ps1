1b5035a92606ed517eb7b8ca2f7c95b6a46accca
Damian Hickey <dhickey@gmail.com>
2018-08-26 20:39:49 +0200

    Replace cake with bullseye and simple-exec.

--- build.ps1
+++ build.ps1
@@ -1,43 +1,13 @@
-<#
-.SYNOPSIS
-This is a Powershell script to bootstrap a Cake build.
-.DESCRIPTION
-This Powershell script will download NuGet if missing, restore NuGet tools (including Cake)
-and execute your Cake build script with the parameters you provide.
-.PARAMETER Target
-The build script target to run.
-.PARAMETER Configuration
-The build configuration to use.
-.PARAMETER Verbosity
-Specifies the amount of information to be displayed.
-.PARAMETER WhatIf
-Performs a dry run of the build script.
-No tasks will be executed.
-.PARAMETER ScriptArgs
-Remaining arguments are added here.
-.LINK
-http://cakebuild.net
-#>
-
 [CmdletBinding()]
 Param(
-    [string]$Target = "Default",
-    [ValidateSet("Release", "Debug")]
-    [string]$Configuration = "Release",
-    [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
-    [string]$Verbosity = "Verbose",
-    [switch]$WhatIf,
-    [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
-    [string[]]$ScriptArgs
+    [string]$Target = "default"
 )
 
-######################
 # Install .NET Core
-######################
 
 $dotNetVersionString = dotnet --version
 $dotNetVersion = [Version]$dotnetVersionString
-$minDotNetVersionString = "2.1.401"
+$minDotNetVersionString = "2.1.400"
 $minDotNetVersion = [Version]$minDotNetVersionString 
 
 If($dotnetVersion -lt $minDotnetVersion){
@@ -50,30 +20,6 @@ else{
     ".Net SDK $dotNetVersionString installed"
 }
 
-##########################
-# Install Cake
-##########################
+# Run Build
 
-Write-Host "Preparing cake..."
-# Make sure tools folder exists
-$toolsPath = Join-Path $PSScriptRoot "tools"
-if (!(Test-Path $toolsPath)) {
-    Write-Host "Creating tools directory..."
-    New-Item -Path $toolsPath -Type directory | out-null
-}
-Invoke-Expression "&dotnet restore tools.csproj --packages tools"
-$cakeDllPath = (Get-ChildItem tools/cake.coreclr/*/Cake.dll)[0].FullName
-
-##########################
-# Run buildscript
-##########################
-Write-Host "Running build script..."
-$arguments = @{
-    target=$Target;
-    configuration=$Configuration;
-    verbosity=$Verbosity;
-    dryrun=$WhatIf;
-    NuGet_UseInProcessClient=$true;
-}.GetEnumerator() | %{"--{0}=`"{1}`"" -f $_.key, $_.value };
-Invoke-Expression "&dotnet $cakeDllPath build.cake $arguments $ScriptArgs";
-exit $LASTEXITCODE
+dotnet run -p build\build.csproj -c Release --project $Target
---
+++ build/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+namespace build
+{
+    class Program
+    {
+        private const string ArtifactsDir = "artifacts";
+        private const string Clean = "clean";
+        private const string Build = "build";
+        private const string RunTests = "run-tests";
+        private const string Pack = "pack";
+        private const string Publish = "publish";
+
+        static void Main(string[] args)
+        {
+            var travisBuildNumber = Environment.GetEnvironmentVariable("TRAVIS_BUILD_NUMBER");
+            var buildNumber = travisBuildNumber ?? "0";
+
+            Target(Clean, () =>
+            {
+                if (Directory.Exists(ArtifactsDir))
+                {
+                    Directory.Delete(ArtifactsDir, true);
+                }
+            });
+
+            Target(Build, () => Run("dotnet", "build ./src/SqlStreamStore.sln -c Release"));
+
+            Target(RunTests, DependsOn(Build), () =>
+            {
+                var projects = new[]
+                {
+                    "SqlStreamStore.Tests",
+                    "SqlStreamStore.MsSql.Tests",
+                    "SqlStreamStore.MsSql.V3.Tests",
+                    "SqlStreamStore.Postgres.Tests",
+                    "SqlStreamStore.Http.Tests"
+                };
+
+                foreach (var project in projects)
+                {
+                    Run("dotnet", $"test src/{project}/{project}.csproj -c Release -r ../../{ArtifactsDir} --no-build -l trx;LogFileName={project}.xml");
+                }
+            });
+
+            Target(Pack, DependsOn(Build), () =>
+            {
+                var versionSuffix = "build" + buildNumber.PadLeft(5, '0');
+                var projects = new[]
+                {
+                    "SqlStreamStore",
+                    "SqlStreamStore.MsSql",
+                    "SqlStreamStore.Postgres",
+                    "SqlStreamStore.Http"
+                };
+
+                foreach (var project in projects)
+                {
+                    Run("dotnet", $"pack src/{project}/{project}.csproj -c Release -o ../../{ArtifactsDir} --no-build --version-suffix {versionSuffix}");
+                }
+            });
+
+            Target(Publish, DependsOn(Pack), () =>
+            {
+                var files = Directory.GetFiles(ArtifactsDir, "*.nupkg", SearchOption.TopDirectoryOnly);
+                Console.WriteLine($"Found packages to publish: {string.Join("; ", files)}");
+
+                var apiKey = Environment.GetEnvironmentVariable("MYGET_API_KEY");
+                if (string.IsNullOrWhiteSpace(apiKey))
+                {
+                    Console.WriteLine("MyGet API key not available. Packages will not be pushed.");
+                    return;
+                }
+
+                foreach (var file in files)
+                {
+                    Run("dotnet", $"nuget publish {file} -s https://www.myget.org/F/sqlstreamstore/api/v3/index.json -k {apiKey}");
+                }
+            });
+
+            Target("default", DependsOn(Clean, RunTests, Publish));
+
+            RunTargets(args);
+        }
+    }
+}

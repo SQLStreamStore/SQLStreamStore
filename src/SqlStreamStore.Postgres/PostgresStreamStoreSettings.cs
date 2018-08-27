1d15c48c8f7a26500c5ec86914bcc9382431903a
thefringeninja <joao.paul@braganca.name>
2018-08-27 15:26:56 +0200

    might as well add code docs here

--- src/SqlStreamStore.Postgres/PostgresStreamStoreSettings.cs
+++ src/SqlStreamStore.Postgres/PostgresStreamStoreSettings.cs
@@ -11,6 +11,10 @@
         private string _schema = "public";
         private Func<string, NpgsqlConnection> _connectionFactory;
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="PostgresStreamStoreSettings"/>.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
         public PostgresStreamStoreSettings(string connectionString)
         {
             Ensure.That(connectionString, nameof(connectionString)).IsNotNullOrWhiteSpace();
@@ -18,11 +22,21 @@
             ConnectionString = connectionString;
         }
 
+        /// <summary>
+        ///     Gets the connection string.
+        /// </summary>
         public string ConnectionString { get; }
 
+        /// <summary>
+        ///     Allows overriding of the stream store notifier. The default implementation
+        ///     creates <see cref="PollingStreamStoreNotifier"/>.
+        /// </summary>
         public CreateStreamStoreNotifier CreateStreamStoreNotifier { get; set; } =
             store => new PollingStreamStoreNotifier(store);
 
+        /// <summary>
+        ///     The schema SQL Stream Store should place database objects into. Defaults to "public".
+        /// </summary>
         public string Schema
         {
             get => _schema;
@@ -34,14 +48,32 @@
             }
         }
 
+        /// <summary>
+        ///     Loads the auto_explain module and turns it on for all queries. Useful for index tuning.
+        /// </summary>
         public bool ExplainAnalyze { get; set; }
 
+        /// <summary>
+        ///     A delegate to return the current UTC now. Used in testing to
+        ///     control timestamps and time related operations.
+        /// </summary>
         public GetUtcNow GetUtcNow { get; set; }
 
+        /// <summary>
+        ///     The log name used for any of the log messages.
+        /// </summary>
         public string LogName { get; set; } = nameof(PostgresStreamStore);
 
+        /// <summary>
+        ///     Allows setting whether or not deleting expired (i.e., older than maxCount) messages happens in the same database transaction as append to stream or not.
+        ///     This does not effect scavenging when setting a stream's metadata - it is always run in the same transaction.
+        /// </summary>
         public bool ScavengeAsynchronously { get; set; } = true;
 
+        /// <summary>
+        ///     Allows overriding the way a <see cref="NpgsqlConnection"/> is created given a connection string.
+        ///     The default implementation simply passes the connection string into the <see cref="NpgsqlConnection"/> constructor.
+        /// </summary>
         public Func<string, NpgsqlConnection> ConnectionFactory
         {
             get => _connectionFactory 

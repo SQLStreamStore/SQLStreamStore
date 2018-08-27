bb70672f089b0e3ebe02d3a68868218f90e6e11a
thefringeninja <joao.paul@braganca.name>
2018-08-27 14:58:28 +0200

    create stream IF updating limits affected no rows

--- src/SqlStreamStore.Postgres/PgSqlScripts/SetStreamMetadata.sql
+++ src/SqlStreamStore.Postgres/PgSqlScripts/SetStreamMetadata.sql
@@ -11,7 +11,7 @@ CREATE OR REPLACE FUNCTION __schema__.set_stream_metadata(
   RETURNS INT AS $F$
 DECLARE
   _current_version INT;
-  _stream_deleted  INT;
+  _stream_updated  INT;
 BEGIN
 
   SELECT current_version
@@ -24,10 +24,25 @@ BEGIN
       )
       INTO _current_version;
 
-  UPDATE __schema__ . streams
+  UPDATE __schema__.streams
   SET max_age   = _max_age,
       max_count = _max_count
   WHERE __schema__.streams.id = _stream_id;
+  GET DIAGNOSTICS _stream_updated = ROW_COUNT;
+
+  IF (_stream_updated = 0)
+  THEN
+    PERFORM __schema__.append_to_stream(
+              _stream_id,
+              _stream_id_original,
+              -1,
+              _created_utc,
+              ARRAY [] :: __schema__.new_stream_message []);
+    UPDATE __schema__.streams
+    SET max_age   = _max_age,
+        max_count = _max_count
+    WHERE __schema__.streams.id = _stream_id;
+  END IF;
 
   RETURN _current_version;
 END;

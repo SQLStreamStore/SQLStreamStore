7203b0996138fd3d7eb7e1a1af7a90c10c5005ad
Adam Ralph <adam@adamralph.com>
2018-09-07 13:13:47 +0200

    address code analysis warnings

--- src/SqlStreamStore.Postgres/PostgresStreamStore.Append.cs
+++ src/SqlStreamStore.Postgres/PostgresStreamStore.Append.cs
@@ -65,7 +65,9 @@
 
                     if(_settings.ScavengeAsynchronously)
                     {
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                         Task.Run(() => TryScavenge(streamIdInfo, cancellationToken), cancellationToken);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
                     }
                     else
                     {

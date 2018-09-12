db41087a2615f3ecb0a4124eb95c38bd89ee0579
thefringeninja <joao.paul@braganca.name>
2018-09-12 14:10:49 +0200

    pass in relevant build environment variables

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --tag sss-build . && docker run --rm --name sss-build -v /var/run/docker.sock:/var/run/docker.sock --network host sss-build dotnet run -p /build/build.csproj -- "$@"
+docker build --tag sss-build . && docker run --rm --name sss-build -v /var/run/docker.sock:/var/run/docker.sock --network host -e TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER -e MYGET_API_KEY=$MYGET_API_KEY sss-build dotnet run -p /build/build.csproj -- "$@"
 

dfed3dc18a411af65d010abf3766f69b312f3c78
Damian Hickey <dhickey@gmail.com>
2018-09-27 00:07:34 +0200

    When appending to a stream with ExpectedVersion.Any and the stream didn't exist, or ExpectedVersion.NoStream, check if metadata already exists and lift the metadata to the stream row.

--- src/SqlStreamStore.MsSql/ScriptsV3/AppendStreamExpectedVersionAny.sql
+++ src/SqlStreamStore.MsSql/ScriptsV3/AppendStreamExpectedVersionAny.sql
@@ -8,8 +8,58 @@ BEGIN TRANSACTION CreateStreamIfNotExists;
         FROM dbo.Streams WITH (UPDLOCK, ROWLOCK, HOLDLOCK)
         WHERE dbo.Streams.Id = @streamId
     )
-        INSERT INTO dbo.Streams (Id, IdOriginal)
-        VALUES (@streamId, @streamIdOriginal);
+        BEGIN
+            INSERT INTO dbo.Streams (Id, IdOriginal)
+            VALUES (@streamId, @streamIdOriginal);
+
+            -- If metadata exists, lift maxAge and maxCount. TODO put this into a function? Duplicate code in Append...NoStream.sql
+            DECLARE @jsonData nvarchar(max);
+            DECLARE @startIndex int;
+            DECLARE @endIndex int;
+            DECLARE @metadataMaxAgeString nvarchar(50);
+            DECLARE @metadataMaxAge int;
+            DECLARE @metadataMaxCountString nvarchar(50);
+            DECLARE @metadataMaxCount int;
+
+            IF EXISTS(SELECT 1 FROM dbo.Streams WITH(NOLOCK)
+                    WHERE dbo.Streams.Id = '$$' + @streamId)
+                BEGIN
+                -- ...read metadata backwards by 1
+                    SELECT TOP(1)
+                        @jsonData = dbo.Messages.JsonData
+                    FROM dbo.Messages
+                INNER JOIN dbo.Streams
+                        ON dbo.Messages.StreamIdInternal = dbo.Streams.IdInternal
+                    WHERE dbo.Streams.Id = '$$' + @streamId
+                ORDER BY dbo.Messages.Position DESC;
+
+                -- Extract MaxAge and MaxCount from Json...
+                    SELECT @startIndex = CHARINDEX('"MaxAge":', @jsonData) + 9;
+                    SELECT @endIndex = CHARINDEX(',', @jsonData, @startIndex);
+                    SELECT @metadataMaxAgeString = SUBSTRING(@jsonData, @startIndex, @endIndex - @startIndex);
+                    SELECT @metadataMaxAge = CASE 
+                                WHEN @metadataMaxAgeString = 'null' 
+                                    THEN NULL 
+                                    ELSE CAST(@metadataMaxAgeString as INT)
+                                END;
+
+                    SELECT @startIndex = CHARINDEX('"MaxCount":', @jsonData) + 11;
+                    SELECT @endIndex = CHARINDEX(',', @jsonData, @startIndex);
+                    SELECT @metadataMaxCountString = SUBSTRING(@jsonData,@startIndex, @endIndex - @startIndex);
+                    SELECT @metadataMaxCount = CASE 
+                                WHEN @metadataMaxCountString = 'null' 
+                                    THEN NULL 
+                                    ELSE CAST(@metadataMaxCountString as INT)
+                                END;
+
+                -- and update the stream row
+                    UPDATE dbo.Streams
+                    SET dbo.Streams.[MaxAge] = @metadataMaxAge,
+                        dbo.Streams.[MaxCount] = @metadataMaxCount
+                    WHERE dbo.Streams.[Id] = '$$' + @streamId
+               
+              END
+        END
 
 COMMIT TRANSACTION CreateStreamIfNotExists;
 

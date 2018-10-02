8e59a45b3055d9297f77b9f582070488da33a990
thefringeninja <joao.paul@braganca.name>
2018-10-02 15:28:21 +0200

    no reason not to use the same container here
    also, there may be issues when running on a non standard port: https://github.com/dotnet/corefx/issues/27307

--- src/SqlStreamStore.MsSql.V3.Tests/MsSqlStreamStoreV3Fixture.cs
+++ src/SqlStreamStore.MsSql.V3.Tests/MsSqlStreamStoreV3Fixture.cs
@@ -137,7 +137,7 @@ namespace SqlStreamStore
             private readonly string _password;
             private const string Image = "microsoft/mssql-server-linux";
             private const string Tag = "2017-CU9";
-            private const int Port = 11433;
+            private const int Port = 1433;
 
             public DockerSqlServerDatabase(string databaseName)
             {
@@ -155,7 +155,7 @@ namespace SqlStreamStore
                     HealthCheck,
                     ports)
                 {
-                    ContainerName = "sql-stream-store-tests-mssql-v3",
+                    ContainerName = "sql-stream-store-tests-mssql",
                     Env = new[] { "ACCEPT_EULA=Y", $"SA_PASSWORD={_password}" }
                 };
             }

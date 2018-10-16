e46155c6b1bca6c30b084de06e90e05a12eedf50
Damian Hickey <dhickey@gmail.com>
2018-10-16 16:13:13 +0200

    Artifacts written to docker host dir (#192)
    Mount a `/artifacts` volume so builds on windows put the artifact on local drive.

--- build/Program.cs
+++ build/Program.cs
@@ -22,9 +22,14 @@ namespace build
 
             Target(Clean, () =>
             {
-                if (Directory.Exists(ArtifactsDir))
+                DirectoryInfo di = new DirectoryInfo(ArtifactsDir);
+                foreach (var file in di.GetFiles())
                 {
-                    Directory.Delete(ArtifactsDir, true);
+                    file.Delete(); 
+                }
+                foreach (var dir in di.GetDirectories())
+                {
+                    dir.Delete(true); 
                 }
             });
 

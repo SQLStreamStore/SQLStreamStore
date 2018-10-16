a6e453452da54ea6f8b462d17174182fa5d907fb
Damian Hickey <dhickey@gmail.com>
2018-10-16 15:44:36 +0200

    Linewrap

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 docker build --tag sss-build .
 docker run --rm --name sss-build \
  -v /var/run/docker.sock:/var/run/docker.sock \
- --network host 
- -e TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER 
+ --network host \
+ -e TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER \
  -e MYGET_API_KEY=$MYGET_API_KEY sss-build \
  dotnet run -p /build/build.csproj -- "$@"

efaaf7e3950a564cc094bff84a2c347da68e22f0
Damian Hickey <dhickey@gmail.com>
2018-10-16 15:49:06 +0200

    Mount artifact volume

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 docker build --tag sss-build .
 docker run --rm --name sss-build \
  -v /var/run/docker.sock:/var/run/docker.sock \
+ -v /artifacts:/artifacts \
  --network host \
  -e TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER \
  -e MYGET_API_KEY=$MYGET_API_KEY sss-build \

b5a7315832da9a8fbcac65f9b375bcb0c3b5da25
Adam Ralph <adam@adamralph.com>
2018-10-29 22:36:01 +0100

    switch to MinVer

--- build.cmd
+++ build.cmd
@@ -2,6 +2,7 @@ docker build --tag sss-build .
 docker run --rm --name sss-build ^
  -v /var/run/docker.sock:/var/run/docker.sock ^
  -v %cd%/artifacts:/artifacts ^
+ -v %cd%/.git:/.git ^
  --network host ^
  sss-build ^
  dotnet run -p /build/build.csproj -- %*

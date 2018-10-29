7d844c11798c8a2de3e26026e80a2ef5e4b595cb
Adam Ralph <adam@adamralph.com>
2018-10-29 22:36:01 +0100

    switch to MinVer

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ docker build --tag sss-build .
 docker run --rm --name sss-build \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v /artifacts:/artifacts \
+ -v $PWD/.git:/.git \
  --network host \
  -e TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER \
  -e MYGET_API_KEY=$MYGET_API_KEY sss-build \

bc18e429b45c20cea4b4db99ed0aea888aa7ffec
Yves Reynhout <yves.reynhout@gmail.com>
2019-10-03 19:45:45 +0200

    Fixed the subscription drop reason of the mysql stream store. (#318)
    * Fixed the subscription drop reason of the mysql stream store.

--- src/SqlStreamStore.MySql/MySqlStreamStore.ReadAll.cs
+++ src/SqlStreamStore.MySql/MySqlStreamStore.ReadAll.cs
@@ -6,6 +6,7 @@ namespace SqlStreamStore
     using System.Data.Common;
     using System.Threading;
     using System.Threading.Tasks;
+    using MySql.Data.MySqlClient;
     using SqlStreamStore.Infrastructure;
     using SqlStreamStore.MySqlScripts;
     using SqlStreamStore.Streams;
@@ -21,66 +22,73 @@ namespace SqlStreamStore
         {
             maxCount = maxCount == int.MaxValue ? maxCount - 1 : maxCount;
 
-            using(var connection = await OpenConnection(cancellationToken))
-            using(var transaction = connection.BeginTransaction())
-            using(var command = BuildStoredProcedureCall(
-                _schema.ReadAll,
-                transaction,
-                Parameters.Count(maxCount + 1),
-                Parameters.Position(fromPositionExclusive),
-                Parameters.ReadDirection(ReadDirection.Forward),
-                Parameters.Prefetch(prefetch)))
-            using(var reader = await command
-                .ExecuteReaderAsync(CommandBehavior.SequentialAccess, cancellationToken)
-                .NotOnCapturedContext())
+            try
             {
-                if(!reader.HasRows)
+                using(var connection = await OpenConnection(cancellationToken))
+                using(var transaction = connection.BeginTransaction())
+                using(var command = BuildStoredProcedureCall(
+                    _schema.ReadAll,
+                    transaction,
+                    Parameters.Count(maxCount + 1),
+                    Parameters.Position(fromPositionExclusive),
+                    Parameters.ReadDirection(ReadDirection.Forward),
+                    Parameters.Prefetch(prefetch)))
+                using(var reader = await command
+                    .ExecuteReaderAsync(CommandBehavior.SequentialAccess, cancellationToken)
+                    .NotOnCapturedContext())
                 {
+                    if(!reader.HasRows)
+                    {
+                        return new ReadAllPage(
+                            fromPositionExclusive,
+                            fromPositionExclusive,
+                            true,
+                            ReadDirection.Forward,
+                            readNext,
+                            Array.Empty<StreamMessage>());
+                    }
+
+                    var messages = new List<(StreamMessage message, int? maxAge)>();
+
+                    while(await reader.ReadAsync(cancellationToken).NotOnCapturedContext())
+                    {
+                        if(messages.Count == maxCount)
+                        {
+                            messages.Add(default);
+                        }
+                        else
+                        {
+                            var streamIdInfo = new StreamIdInfo(reader.GetString(0));
+                            var (message, maxAge, _) =
+                                await ReadAllStreamMessage(reader, streamIdInfo.MySqlStreamId, prefetch);
+                            messages.Add((message, maxAge));
+                        }
+                    }
+
+                    bool isEnd = true;
+
+                    if(messages.Count == maxCount + 1) // An extra row was read, we're not at the end
+                    {
+                        isEnd = false;
+                        messages.RemoveAt(maxCount);
+                    }
+
+                    var filteredMessages = FilterExpired(messages);
+
+                    var nextPosition = filteredMessages[filteredMessages.Count - 1].Position + 1;
+
                     return new ReadAllPage(
                         fromPositionExclusive,
-                        fromPositionExclusive,
-                        true,
+                        nextPosition,
+                        isEnd,
                         ReadDirection.Forward,
                         readNext,
-                        Array.Empty<StreamMessage>());
+                        filteredMessages.ToArray());
                 }
-
-                var messages = new List<(StreamMessage message, int? maxAge)>();
-
-                while(await reader.ReadAsync(cancellationToken).NotOnCapturedContext())
-                {
-                    if(messages.Count == maxCount)
-                    {
-                        messages.Add(default);
-                    }
-                    else
-                    {
-                        var streamIdInfo = new StreamIdInfo(reader.GetString(0));
-                        var (message, maxAge, _) =
-                            await ReadAllStreamMessage(reader, streamIdInfo.MySqlStreamId, prefetch);
-                        messages.Add((message, maxAge));
-                    }
-                }
-
-                bool isEnd = true;
-
-                if(messages.Count == maxCount + 1) // An extra row was read, we're not at the end
-                {
-                    isEnd = false;
-                    messages.RemoveAt(maxCount);
-                }
-
-                var filteredMessages = FilterExpired(messages);
-
-                var nextPosition = filteredMessages[filteredMessages.Count - 1].Position + 1;
-
-                return new ReadAllPage(
-                    fromPositionExclusive,
-                    nextPosition,
-                    isEnd,
-                    ReadDirection.Forward,
-                    readNext,
-                    filteredMessages.ToArray());
+            }
+            catch(MySqlException exception) when(exception.InnerException is ObjectDisposedException disposedException)
+            {
+                throw new ObjectDisposedException(disposedException.Message, exception);
             }
         }
 
@@ -94,65 +102,72 @@ namespace SqlStreamStore
             maxCount = maxCount == int.MaxValue ? maxCount - 1 : maxCount;
             var ordinal = fromPositionExclusive == Position.End ? long.MaxValue - 1 : fromPositionExclusive;
 
-            using(var connection = await OpenConnection(cancellationToken))
-            using(var transaction = connection.BeginTransaction())
-            using(var command = BuildStoredProcedureCall(
-                _schema.ReadAll,
-                transaction,
-                Parameters.Count(maxCount + 1),
-                Parameters.Position(ordinal),
-                Parameters.ReadDirection(ReadDirection.Backward),
-                Parameters.Prefetch(prefetch)))
-            using(var reader = await command
-                .ExecuteReaderAsync(CommandBehavior.SequentialAccess, cancellationToken)
-                .NotOnCapturedContext())
+            try
             {
-                if(!reader.HasRows)
+                using(var connection = await OpenConnection(cancellationToken))
+                using(var transaction = connection.BeginTransaction())
+                using(var command = BuildStoredProcedureCall(
+                    _schema.ReadAll,
+                    transaction,
+                    Parameters.Count(maxCount + 1),
+                    Parameters.Position(ordinal),
+                    Parameters.ReadDirection(ReadDirection.Backward),
+                    Parameters.Prefetch(prefetch)))
+                using(var reader = await command
+                    .ExecuteReaderAsync(CommandBehavior.SequentialAccess, cancellationToken)
+                    .NotOnCapturedContext())
                 {
-                    // When reading backwards and there are no more items, then next position is LongPosition.Start,
-                    // regardless of what the fromPosition is.
+                    if(!reader.HasRows)
+                    {
+                        // When reading backwards and there are no more items, then next position is LongPosition.Start,
+                        // regardless of what the fromPosition is.
+                        return new ReadAllPage(
+                            Position.Start,
+                            Position.Start,
+                            true,
+                            ReadDirection.Backward,
+                            readNext,
+                            Array.Empty<StreamMessage>());
+                    }
+
+                    var messages = new List<(StreamMessage message, int? maxAge)>();
+
+                    long lastOrdinal = 0;
+                    while(await reader.ReadAsync(cancellationToken).NotOnCapturedContext())
+                    {
+                        var streamIdInfo = new StreamIdInfo(reader.GetString(0));
+                        var (message, maxAge, position) =
+                            await ReadAllStreamMessage(reader, streamIdInfo.MySqlStreamId, prefetch);
+                        messages.Add((message, maxAge));
+
+                        lastOrdinal = position;
+                    }
+
+                    bool isEnd = true;
+                    var nextPosition = lastOrdinal;
+
+                    if(messages.Count == maxCount + 1) // An extra row was read, we're not at the end
+                    {
+                        isEnd = false;
+                        messages.RemoveAt(maxCount);
+                    }
+
+                    var filteredMessages = FilterExpired(messages);
+
+                    fromPositionExclusive = filteredMessages.Count > 0 ? filteredMessages[0].Position : 0;
+
                     return new ReadAllPage(
-                        Position.Start,
-                        Position.Start,
-                        true,
+                        fromPositionExclusive,
+                        nextPosition,
+                        isEnd,
                         ReadDirection.Backward,
                         readNext,
-                        Array.Empty<StreamMessage>());
+                        filteredMessages.ToArray());
                 }
-
-                var messages = new List<(StreamMessage message, int? maxAge)>();
-
-                long lastOrdinal = 0;
-                while(await reader.ReadAsync(cancellationToken).NotOnCapturedContext())
-                {
-                    var streamIdInfo = new StreamIdInfo(reader.GetString(0));
-                    var (message, maxAge, position) =
-                        await ReadAllStreamMessage(reader, streamIdInfo.MySqlStreamId, prefetch);
-                    messages.Add((message, maxAge));
-
-                    lastOrdinal = position;
-                }
-
-                bool isEnd = true;
-                var nextPosition = lastOrdinal;
-
-                if(messages.Count == maxCount + 1) // An extra row was read, we're not at the end
-                {
-                    isEnd = false;
-                    messages.RemoveAt(maxCount);
-                }
-
-                var filteredMessages = FilterExpired(messages);
-
-                fromPositionExclusive = filteredMessages.Count > 0 ? filteredMessages[0].Position : 0;
-
-                return new ReadAllPage(
-                    fromPositionExclusive,
-                    nextPosition,
-                    isEnd,
-                    ReadDirection.Backward,
-                    readNext,
-                    filteredMessages.ToArray());
+            }
+            catch(MySqlException exception) when(exception.InnerException is ObjectDisposedException disposedException)
+            {
+                throw new ObjectDisposedException(disposedException.Message, exception);
             }
         }
 

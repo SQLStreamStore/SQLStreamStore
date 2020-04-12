ff991591c99ddc67672698a2849f8fca5a6a8e27
rvdkooy <ronald@sharpsolutions.nl>
2020-04-12 23:02:25 +0200

    refactored spec

--- tests/SqlStreamStore.AcceptanceTests/AcceptanceTests.ReadStream.cs
+++ tests/SqlStreamStore.AcceptanceTests/AcceptanceTests.ReadStream.cs
@@ -376,22 +376,18 @@
         public async Task When_reading_a_stream_backwards_with_gaps_and_providing_the_fromVersion_explicitly()
         {
             var streamId = new StreamId("streamid");
-            var messageOne = new NewStreamMessage(Guid.NewGuid(), "type", "jsondata1");
-            var messageTwo = new NewStreamMessage(Guid.NewGuid(), "type", "jsondata2");
-            var messageThree = new NewStreamMessage(Guid.NewGuid(), "type", "jsondata3");
+            var messages = CreateNewStreamMessages(1, 2, 3);
+            var firstMessageId = messages[0].MessageId;
+            var secondMessageId = messages[1].MessageId;
+            var ThirdMessageId = messages[2].MessageId;
 
-            var ap1 = await Store.AppendToStream(streamId, ExpectedVersion.Any, messageOne);
-            var ap2 = await Store.AppendToStream(streamId, ExpectedVersion.Any, messageTwo);
-            var ap3 = await Store.AppendToStream(streamId, ExpectedVersion.Any, messageThree);
+            var appendResult = await Store.AppendToStream(streamId, ExpectedVersion.Any, messages);
+            await Store.DeleteMessage(streamId, secondMessageId);
 
-            await Store.DeleteMessage(streamId, messageTwo.MessageId);
-
-            var page = await Store.ReadStreamBackwards(streamId, ap3.CurrentVersion, 10);
+            var page = await Store.ReadStreamBackwards(streamId, appendResult.CurrentVersion, 10);
             page.Messages.Length.ShouldBe(2);
-            var jsonDataOfMessageOne = await page.Messages.First().GetJsonData();
-            var jsonDataOfMessageThree = await page.Messages.Last().GetJsonData();
-            jsonDataOfMessageOne.ShouldBe("jsondata3");
-            jsonDataOfMessageThree.ShouldBe("jsondata1");
+            page.Messages.First().MessageId.ShouldBe(ThirdMessageId);
+            page.Messages.Last().MessageId.ShouldBe(firstMessageId);
         }
 
         // ReSharper disable once UnusedMethodReturnValue.Global

26c803b179f4cc795582b31807f3c4243e83f7c0
Damian Hickey <dhickey@gmail.com>
2020-05-18 14:13:18 +0200

    Tryout running mssql, mysql and postgres as services via GHA yaml

--- tests/SqlStreamStore.TestUtils/MsSql/SqlServerContainer.cs
+++ tests/SqlStreamStore.TestUtils/MsSql/SqlServerContainer.cs
@@ -14,7 +14,7 @@
         private readonly string _databaseName;
         private IContainerService _containerService;
         private const string ContainerName = "sql-stream-store-tests-mssql";
-        private const string Password = "!Passw0rd";
+        private const string Password = "E@syP@ssw0rd";
         private const string Image = "microsoft/mssql-server-linux:2017-CU9";
         private const int HostPort = 11433;
         private const int ContainerPort = 1433;
